1edb38ed94ccc62eee82549aab6cae4837b6e307
Ekaterina Yurina <komsa_9191@mail.ru>
2025-05-07 13:26:57 +1000

    feat: add functions to index.js

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+import { getMonthAndYear } from './calculations.js';
+import { clickHandlerButtonGenerate, clickHandlerCell, clickHandlerButtonAdd } from './eventHandlers.js';
+
+getMonthAndYear();
+clickHandlerButtonGenerate();
+clickHandlerCell();
+clickHandlerButtonAdd();
